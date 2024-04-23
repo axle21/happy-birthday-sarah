cf8a0f66d2513ba6e1ace456b002b259264fd797
Axle Rodriguez <rodriguez.imaxle@gmail.com>
2024-04-23 16:35:19 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,46 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Happy Birthday to the brightest star today! 🌟",
+        "On this special day, ",
+        "I want to celebrate the incredible person you are",
+        "your kindness",
+        "your Sweetness", 
+        "and your beautiful spirit",
+        "that lights up every room you enter.",
+        "May your day be filled with ",
+        "joy",
+        "laughter",
+        "and all the love you deserve.",
+        "Here's to another year of amazing adventures",
+        "unforgettable memories",
+        "and endless smiles",
+        "Cheers to you! 🎉🎂💖",
+        "Again",
+        "Happy Birthday, SARAH! 🎈🎁🎊",
+        "and maybe see you soon",
+        "profite de ta journée et joyeux 30  🎉🎂💖😝",
+      ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Lights On",
+        play: "Play Music",
+        bannar_coming: "Show Banner",
+        balloons_flying: "Fly the balloons",
+        cake_fadein: "Bring the Cake？",
+        light_candle: "Light the Candle",
+        wish_message: "Happy Birthday :).",
+        story: "A MESSAGE FOR YOU",
+    }
+};
